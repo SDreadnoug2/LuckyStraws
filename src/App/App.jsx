f3031cd7c470ac6a5ce0e81110e483130383be6d
Nick <nick@Nicks-MBP.lan>
2024-08-09 12:35:39 -0400

    update

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -7,8 +7,6 @@ import { ActiveTabContext } from '../contexts/activeTabContext';
 function App() {
 
   const [activeTab, setActiveTab] = useState('')
-  const calendarInfo = ["August 8th - Open Mic", "August 9th - Open Mic", "August 8th - Open Mic", "August 9th - Open Mic", "August 8th - Open Mic", "August 9th - Open Mic", "August 8th - Open Mic", "August 9th - Open Mic"]
-// const calendarInfo = ["August 8th - Open Mic", "August 9th - Open Mic", "August 8th - Open Mic", "August 9th - Open Mic", "August 8th - Open Mic", "August 9th - Open Mic", "August 8th - Open Mic", "August 9th - Open Mic"]
   return (
     <ActiveTabContext.Provider  value= {{activeTab, setActiveTab}}>
       <div className='App'>
